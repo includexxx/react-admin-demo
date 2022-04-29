e45c298f120b60296ca5d07d96ddaac3235d2ced
Mahmudul Hasan <mriyad94@gmail.com>
2022-04-29 08:31:30 +0600

    first commit

---
+++ src/fakeServer/index.ts
@@ -0,0 +1,14 @@
+// only install the mocks once
+// this is necessary with react@18 in StrictMode
+let fakeServer: any;
+
+export default (type: string) => {
+    if (!fakeServer) {
+       
+        fakeServer = import('./rest').then(factory =>
+            factory.default()
+        );
+        
+    }
+    return fakeServer;
+};
